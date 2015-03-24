9a0238900ca1e223de1ff6d45c5b3fd3b6ebabc0
Dave Gurnell <dave@untyped.com>
2015-03-24 17:13:06 +0000

    Added NPM build in `./lib`

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 npm install
 
 mkdir -p ./dist
+babel ./src --out-dir ./lib --modules commonStrict
 browserify --require ./src/index.js:compose --outfile dist/index.js --transform babelify
 browserify --require ./src/index.js:compose --require ./src/demo.js:demo --outfile dist/demo.js  --transform babelify
